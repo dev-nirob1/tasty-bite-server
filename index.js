b7320eb8105b8cdaa7176310a3966ef22f484f2b
dev-nirob1 <dev-nirob1@gmail.com>
2023-06-26 08:11:51 +0600

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = 5000
 
+app.use(cors())
 app.get('/', (req, res) => {
-    res.send('Hello World! i am nirob')
+    res.send('Hello World! i am nirob i am a web developer amader desher nam bangladesh')
+    
 })
 
 
