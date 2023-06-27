68221de3fa15942a9a8db6ddd811cfdfd700c6d1
dev-nirob1 <dev-nirob1@gmail.com>
2023-06-27 15:36:46 +0600

    json file  updated

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-    
+
 })
 app.get('/chefs', (req, res) => {
     res.send(chefData)
 })
 
 app.get('/chefs/:id', (req, res) => {
+    const id = parseInt(req.params.id)
     const selectedChef = chefData.find(singleChef => singleChef.id === id);
     res.send(selectedChef);
 })
@@ -23,4 +24,4 @@ app.get('/chefs/:id', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
